d8209b40497bd5a53140c8869ef71e81c0631d75
Shahbaz T <shahbaztariq2049@gmail.com>
2025-05-26 03:00:38 +0500

    add rolling forecast option (halfway complete)

--- Ethereum Price Prediction.py
+++ Ethereum Price Prediction.py
@@ -18,23 +18,47 @@ eth_data.columns.name = None
 
 print(f'Empty Cells: {eth_data.isna().sum()}')
 print(f'Null Values: {eth_data.isnull().sum()}')
-print('\nStatistical Description:\n')
+print('\n' + '--'*30 + 'Statistical Description:' + '--'*30 + '\n')
 print(eth_data.describe())
 
 today = pd.Timestamp.today().normalize()
+backtest_dates = pd.date_range(end=today - pd.Timestamp(days=1), periods=7)
 
 eth_prices = eth_data['Close']
-train_prices = eth_prices[eth_prices.index < today]
+arima_order = (0,1,0)
+p = 5
+q = 0
 
-my_order = (0,1,0)
-arima = ARIMA(train_prices, order=my_order).fit()
-forecast_price = arima.forecast(steps=1).iloc[0]
+actual_prices = []
+forecasted_prices = []
+residuals = []
+volatilities = []
 
-train_residuals = (train_prices - arima.fittedvalues).dropna()
+for end_date in backtest_dates:
+    
+    train_prices = eth_prices[eth_prices.index < end_date]
+    
+    arima = ARIMA(train_prices, order=arima_order).fit()
+    forecast_price = arima.forecast(steps=1).iloc[0]
+    
+    if end_date in eth_prices.index:
+        actual_price = eth_prices.loc[end_date]
+    else:
+        continue
+    
+    residual = actual_price - forecast_price
+    
+    train_residuals = (train_prices - arima.fittedvalues).dropna()
+    arch = arch_model(train_residuals, p=p, q=q).fit(disp='off')
+    volatility = np.sqrt(arch.forecast(horizon=1).variance.iloc[-1,0])
 
-arch = arch_model(train_residuals, p=5, q=0).fit(disp='off')
-volatility = np.sqrt(arch.forecast(horizon=1).variance.iloc[-1,0])
+    actual_prices.append(actual_price)
+    forecasted_prices.append(forecast_price)
+    residuals.append(residual)
+    volatilities.append(volatility)
 
+
+# Log data into a dataframe
 forecast_log = pd.DataFrame({'Date': [today],
                              'Close Price': [forecast_price],
                              'Volatility': [volatility]})
@@ -53,6 +77,7 @@ if os.path.exists(log_file):
 else:
     forecast_log.to_csv(log_file, mode='w', header=True)
     print("Forecast log created.")
+
 
 
 
